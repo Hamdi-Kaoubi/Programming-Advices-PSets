1bde7e7a48e624eb7d4ffd58bc33f9e2498e1b4d
Coding-Senshi <114572333+Hamdi-Kaoubi@users.noreply.github.com>
2025-03-16 13:09:13 +0100

    Add files via upload

---
+++ 08 - Algorithms & Problem-Solving Level 4/problem04-numofdhms-inyear.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+struct stDaysHrsMinsSecs
+{
+    int days;
+    int hours;
+    int munites;
+    int seconds;
+};
+
+int ReadYear()
+{
+    int year;
+    cout << "enter a year : ";
+    cin >> year;
+    return year;
+}
+
+bool CheckLeap(int year)
+{
+    return (year % 400 == 0) or (year % 100 != 0 and year % 4 == 0);
+}
+
+stDaysHrsMinsSecs FillYear(int Year)
+{
+    stDaysHrsMinsSecs DaysHoursMinutesSeconds;
+    if (CheckLeap(Year))
+    {
+        DaysHoursMinutesSeconds.days = 366;
+    }
+    else
+        DaysHoursMinutesSeconds.days = 365;
+    DaysHoursMinutesSeconds.hours = DaysHoursMinutesSeconds.days * 24;
+    DaysHoursMinutesSeconds.munites = DaysHoursMinutesSeconds.hours * 60;
+    DaysHoursMinutesSeconds.seconds = DaysHoursMinutesSeconds.munites * 60;
+    return DaysHoursMinutesSeconds;
+}
+
+
+
+
+int main()
+{
+    int Year = ReadYear();
+    stDaysHrsMinsSecs DaysHoursMinutesSeconds = FillYear(Year);
+    cout << "\nNumber of days    in year [" << Year << "] is " << DaysHoursMinutesSeconds.days << "\n";
+    cout << "Number of hours   in year [" << Year << "] is " << DaysHoursMinutesSeconds.hours << "\n";
+    cout << "Number of munites in year [" << Year << "] is " << DaysHoursMinutesSeconds.munites << "\n";
+    cout << "Number of seconds in year [" << Year << "] is " << DaysHoursMinutesSeconds.seconds << "\n";
+}
+
