dff18bce74604f58ac8dcc954d8440aeee17228a
Coding-Senshi <114572333+Hamdi-Kaoubi@users.noreply.github.com>
2025-03-18 14:52:47 +0100

    Add files via upload

---
+++ 08 - Algorithms & Problem-Solving Level 4/problem14-date1-equals-date2.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+struct sDate
+{
+    short Day;
+    short Month;
+    short Year;
+};
+short ReadYear()
+{
+    short year;
+    cout << "\nPlease enter a year : ";
+    cin >> year;
+    return year;
+}
+short ReadMonth()
+{
+    short month;
+    cout << "\nPlease enter a month : ";
+    cin >> month;
+    return month;
+}
+short ReadDay()
+{
+    short day;
+    cout << "\nPlease enter a day : ";
+    cin >> day;
+    return day;
+}
+sDate ReadFullDate()
+{
+    sDate Date;
+    Date.Day = ReadDay();
+    Date.Month = ReadMonth();
+    Date.Year = ReadYear();
+    return Date;
+}
+
+bool CheckLeap(short year)
+{
+    return (year % 400 == 0) or (year % 100 != 0 and year % 4 == 0);
+}
+short DaysInMonth(short year, short month)
+{
+    if (month < 1 or month > 12)
+        return 0;
+    short DaysOfMonth[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    return (month == 2) ? (CheckLeap(year) ? 29 : 28) : DaysOfMonth[month - 1];
+}
+short DaysFromBeginning(short year, short month, short day)
+{
+    short FromBeginning = 0;
+    for (short i = 1; i < month; i++)
+    {
+        FromBeginning += DaysInMonth(year, i);
+    }
+    FromBeginning += day;
+    return FromBeginning;
+}
+
+bool CheckDates(sDate Date1, sDate Date2)
+{
+    return (Date1.Year == Date2.Year) ? ((Date1.Month == Date2.Month) ? Date1.Day == Date2.Day ? true : false : false) : false;
+}
+
+
+
+
+
+
+int main()
+{
+    sDate Date1 = ReadFullDate();
+    sDate Date2 = ReadFullDate();
+    if (CheckDates(Date1, Date2))
+        cout << "\nYes, Date 1 is equal to Date 2.\n";
+    else
+        cout << "\nNo, Date 1 is NOT equal to Date 2.\n";
+}
+
