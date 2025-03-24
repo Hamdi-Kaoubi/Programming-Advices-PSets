5f52c6780a34b5fafae865482cd645a5093f7fdd
Coding-Senshi <114572333+Hamdi-Kaoubi@users.noreply.github.com>
2025-03-24 13:59:59 +0100

    Add files via upload

---
+++ 08 - Algorithms & Problem-Solving Level 4/problem62-validate-date.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+struct sDate
+{
+    short Day;
+    short Month;
+    short Year;
+};
+short ReadYear()
+{
+    short year;
+    cout << "Please enter a year : ";
+    cin >> year;
+    return year;
+}
+short ReadMonth()
+{
+    short month;
+    cout << "Please enter a month : ";
+    cin >> month;
+    return month;
+}
+short ReadDay()
+{
+    short day;
+    cout << "\nPlease enter a day : ";
+    cin >> day;
+    return day;
+}
+sDate ReadFullDate()
+{
+    sDate Date;
+    Date.Day = ReadDay();
+    Date.Month = ReadMonth();
+    Date.Year = ReadYear();
+    return Date;
+}
+
+bool CheckLeap(short year)
+{
+    return (year % 400 == 0) or (year % 100 != 0 and year % 4 == 0);
+}
+short DaysInMonth(short year, short month)
+{
+    if (month < 1 or month > 12)
+        return 0;
+    short DaysOfMonth[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    return (month == 2) ? (CheckLeap(year) ? 29 : 28) : DaysOfMonth[month - 1];
+}
+
+bool IsDateValid(sDate Date)
+{
+    return (Date.Day >= 1 && Date.Day <= DaysInMonth(Date.Year, Date.Month)) && (Date.Month >= 1 && Date.Month <= 12);
+}
+
+
+
+
+int main()
+{
+    sDate Date = ReadFullDate();
+    if (IsDateValid(Date))
+        cout << "\nYes, " << Date.Day << "/" << Date.Month << "/" << Date.Year << " is Valid Date.\n";
+    else
+        cout << "\nNO, " << Date.Day << "/" << Date.Month << "/" << Date.Year << " is NOT Valid Date.\n";
+}
